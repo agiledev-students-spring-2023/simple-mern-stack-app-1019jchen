20012cf9c677a2d942133955fd924726d5471f76
James Chen <jyc8938@stern.nyu.edu>
2023-02-15 00:56:54 -0500

    Removed Extra Text

--- back-end/app.js
+++ back-end/app.js
@@ -47,8 +47,7 @@ app.get('/aboutus', (req, res) => {
     paragraphs: [
       "Hi! I'm James. I'm a dynamic and dedicated individual who is pursuing a dual degree in Computer Science and Finance at New York University. I am a talented student and demonstrate my passion for learning and my ability to excel in multiple areas.",
       "I am also an incoming Software Engineering Intern at McDonald's Corporation, where I will have the opportunity to apply my skills and knowledge to real-world projects and gain valuable industry experience. I am an exceptional communicator and problem solver, and I've already demonstrated my abilities through my work as a TA for Graduate Statistics at Stern and as a Research Assistant in Cryptocurrency and Decentralized Finance.",
-      "In my free time, I enjoy skiing, exercising, and listening to music. I am also a foodie who loves trying new foods and is always on the lookout for his next culinary adventure.",
-      "future URL"
+      "In my free time, I enjoy skiing, exercising, and listening to music. I am also a foodie who loves trying new foods and is always on the lookout for his next culinary adventure."
   ],
     imageURL: "/photo.jpg"
   })
